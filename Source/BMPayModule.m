a2b627aa97a55aefd1d4dbb1957a58bcc613f2af
dmlzj <284832506@qq.com>
2020-04-07 16:41:49 +0800

    升级微信sdk

--- Source/BMPayModule.m
+++ Source/BMPayModule.m
@@ -22,26 +22,35 @@ WX_PlUGIN_EXPORT_MODULE(bmWXPay, BMPayModule)
 @synthesize weexInstance;
 
 WX_EXPORT_METHOD_SYNC(@selector(isInstallWXApp))
-WX_EXPORT_METHOD_SYNC(@selector(initWX:))
+WX_EXPORT_METHOD_SYNC(@selector(initWX:universalLink:))
 WX_EXPORT_METHOD(@selector(pay:callback:))
 
 /** 判断是否安装了微信 */
 -(BOOL)isInstallWXApp
 {
-    dispatch_sync(dispatch_get_main_queue(), ^{
-        self.WXAppIsInstall = [WXApi isWXAppInstalled];
+  dispatch_sync(dispatch_get_main_queue(), ^{
+      self.WXAppIsInstall = [WXApi isWXAppInstalled];
     });
-    return self.WXAppIsInstall;
+  return self.WXAppIsInstall;
 }
 
-- (void)initWX:(NSString *)appkey
+- (void)initWX:(NSString *)appkey universalLink:(NSString *)universalLink
 {
-    [WXApi registerApp:appkey];
+  [WXApi startLogByLevel:WXLogLevelDetail logBlock:^(NSString *log) {
+      NSLog(@"WeChatSDK: %@", log);
+    }];
+
+  [WXApi registerApp:appkey universalLink:universalLink];
+
+  //调用自检函数
+  [WXApi checkUniversalLinkReady:^(WXULCheckStep step, WXCheckULStepResult* result) {
+      NSLog(@"%@, %u, %@, %@", @(step), result.success, result.errorInfo, result.suggestion);
+    }];
 }
 
 - (void)pay:(NSDictionary *)info callback:(WXModuleCallback)callback
 {
-    [[BMPayManager shareInstance] payByWechat:info callback:callback];
+  [[BMPayManager shareInstance] payByWechat:info callback:callback];
 }
 
 @end
